a06748382093c4babd81c7b5d06a039efd21966c
Zoltan Csenyi <cszoltan422@gmail.com>
2019-02-02 23:54:24 +0100

    Delete update-db.sh

--- update-db.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-java -version
-source ~/.profile
-echo $DB_UPDATER_HOME
-cd ${DB_UPDATER_HOME}
-mvn spring-boot:run -Dspring.profiles.active=$1 -Dtimetable.resource.selected-buses=$2
