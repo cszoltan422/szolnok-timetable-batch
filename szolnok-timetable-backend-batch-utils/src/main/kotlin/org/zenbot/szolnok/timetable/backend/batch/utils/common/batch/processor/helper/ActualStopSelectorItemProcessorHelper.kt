2efd5ced022484a40bebdd7f076ac0526ef95aba
Zoltan Csenyi <zoltan_csenyi@epam.com>
2019-10-11 11:38:25 +0200

    - Add unit tests for ActualStopSelectorItemProcessorHelper

--- szolnok-timetable-backend-batch-utils/src/main/kotlin/org/zenbot/szolnok/timetable/backend/batch/utils/common/batch/processor/helper/ActualStopSelectorItemProcessorHelper.kt
+++ szolnok-timetable-backend-batch-utils/src/main/kotlin/org/zenbot/szolnok/timetable/backend/batch/utils/common/batch/processor/helper/ActualStopSelectorItemProcessorHelper.kt
@@ -5,20 +5,61 @@ import org.springframework.stereotype.Component
 import org.zenbot.szolnok.timetable.backend.batch.utils.common.properties.TimetableSelectorProperties
 import org.zenbot.szolnok.timetable.backend.batch.utils.common.util.StringCleaner
 
+/**
+ * Selects the actual stop of the timetable from an HTML document
+ */
 @Component
 class ActualStopSelectorItemProcessorHelper(private val stringCleaner: StringCleaner) {
 
+    /**
+     * @param htmlDocument The HTML document to select the actual stop from
+     * @param selectorProperties The CSS selectors encapsulated in a property object
+     * @return The actual stop of the timetable HTML page. Note that in the HTML, the actual stop
+     *          is surrounded by parentheses, like "(BUS STOP)"
+     */
     fun getActualStop(htmlDocument: Document, selectorProperties: TimetableSelectorProperties): String {
-        val actualStopSelector = selectorProperties.actualStopSelector
-        var actualStop = htmlDocument.select(actualStopSelector).text()
-        // In the html page they put it insede () signs.
-        actualStop = actualStop.subSequence(actualStop.indexOf("(") + 1, actualStop.indexOf(")")) as String
-        if (actualStop.contains("(")) {
-            actualStop += ")"
-        }
-        if (actualStop.endsWith(".")) {
-            actualStop = actualStop.substring(0, actualStop.length - 1)
-        }
+        var actualStop = getHtmlText(selectorProperties, htmlDocument)
+        actualStop = removeParentheses(actualStop)
+        actualStop = addParenthesesIfStopShouldContain(actualStop)
+        actualStop = removeDotFromText(actualStop)
+        return cleanString(actualStop)
+    }
+
+    private fun cleanString(actualStop: String): String {
         return stringCleaner.clean(actualStop)
+    }
+
+    private fun removeDotFromText(actualStop: String): String {
+        var actualStop1 = actualStop
+        if (actualStop1.endsWith(DOT)) {
+            actualStop1 = actualStop1.substring(0, actualStop1.length - 1)
+        }
+        return actualStop1
+    }
+
+    private fun addParenthesesIfStopShouldContain(actualStop: String): String {
+        var actualStop1 = actualStop
+        if (actualStop1.contains(OPEN_PARENTHESES)) {
+            actualStop1 += CLOSE_PARENTHESES
+        }
+        return actualStop1
+    }
+
+    private fun removeParentheses(actualStop: String): String {
+        var actualStop1 = actualStop
+        actualStop1 = actualStop1.subSequence(actualStop1.indexOf(OPEN_PARENTHESES) + 1, actualStop1.indexOf(CLOSE_PARENTHESES)) as String
+        return actualStop1
+    }
+
+    private fun getHtmlText(selectorProperties: TimetableSelectorProperties, htmlDocument: Document): String {
+        val actualStopSelector = selectorProperties.actualStopSelector
+        val actualStop = htmlDocument.select(actualStopSelector).text()
+        return actualStop
+    }
+
+    companion object {
+        val OPEN_PARENTHESES = "("
+        val CLOSE_PARENTHESES = ")"
+        val DOT = "."
     }
 }
---
+++ szolnok-timetable-backend-batch-utils/src/test/kotlin/org/zenbot/szolnok/timetable/backend/batch/utils/common/batch/processor/helper/ActualStopSelectorItemProcessorHelperTest.kt
@@ -0,0 +1,101 @@
+package org.zenbot.szolnok.timetable.backend.batch.utils.common.batch.processor.helper
+
+import org.assertj.core.api.Assertions.assertThat
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.BDDMockito.anyString
+import org.mockito.BDDMockito.given
+import org.mockito.BDDMockito.verify
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
+import org.zenbot.szolnok.timetable.backend.batch.utils.common.properties.TimetableSelectorProperties
+import org.zenbot.szolnok.timetable.backend.batch.utils.common.util.StringCleaner
+
+class ActualStopSelectorItemProcessorHelperTest {
+
+    @InjectMocks
+    private lateinit var testSubject : ActualStopSelectorItemProcessorHelper
+
+    @Mock
+    private lateinit var stringCleaner: StringCleaner
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun `getActualStop should remove the parentheses around the string value and clean it with the cleaner`() {
+        // GIVEN
+        val htmlDocument = mock(Document::class.java)
+        val elements = mock(Elements::class.java)
+        val timetableSelectorProperties = TimetableSelectorProperties()
+        timetableSelectorProperties.actualStopSelector = "selector"
+
+        given(htmlDocument.select(ArgumentMatchers.anyString())).willReturn(elements)
+        given(elements.text()).willReturn("(STOP)")
+        given(stringCleaner.clean(ArgumentMatchers.anyString())).willReturn("cleaned")
+
+        // WHEN
+        val result = testSubject.getActualStop(htmlDocument, timetableSelectorProperties)
+
+        // THEN
+        Mockito.verify(htmlDocument).select(timetableSelectorProperties.actualStopSelector)
+        Mockito.verify(elements).text()
+        Mockito.verify(stringCleaner).clean("STOP")
+
+        assertThat(result).isEqualTo("cleaned")
+    }
+
+    @Test
+    fun `getActualStop should add parentheses to the end of the string value if required`() {
+        // GIVEN
+        val htmlDocument = mock(Document::class.java)
+        val elements = mock(Elements::class.java)
+        val timetableSelectorProperties = TimetableSelectorProperties()
+        timetableSelectorProperties.actualStopSelector = "selector"
+
+        given(htmlDocument.select(ArgumentMatchers.anyString())).willReturn(elements)
+        given(elements.text()).willReturn("(STOP (VALUE))")
+        given(stringCleaner.clean(ArgumentMatchers.anyString())).willReturn("cleaned")
+
+        // WHEN
+        val result = testSubject.getActualStop(htmlDocument, timetableSelectorProperties)
+
+        // THEN
+        Mockito.verify(htmlDocument).select(timetableSelectorProperties.actualStopSelector)
+        Mockito.verify(elements).text()
+        Mockito.verify(stringCleaner).clean("STOP (VALUE)")
+
+        assertThat(result).isEqualTo("cleaned")
+    }
+
+    @Test
+    fun `getActualStop should remove the dot from the end of the string if required`() {
+        // GIVEN
+        val htmlDocument = mock(Document::class.java)
+        val elements = mock(Elements::class.java)
+        val timetableSelectorProperties = TimetableSelectorProperties()
+        timetableSelectorProperties.actualStopSelector = "selector"
+
+        given(htmlDocument.select(ArgumentMatchers.anyString())).willReturn(elements)
+        given(elements.text()).willReturn("(STOP.))")
+        given(stringCleaner.clean(ArgumentMatchers.anyString())).willReturn("cleaned")
+
+        // WHEN
+        val result = testSubject.getActualStop(htmlDocument, timetableSelectorProperties)
+
+        // THEN
+        Mockito.verify(htmlDocument).select(timetableSelectorProperties.actualStopSelector)
+        Mockito.verify(elements).text()
+        Mockito.verify(stringCleaner).clean("STOP")
+
+        assertThat(result).isEqualTo("cleaned")
+    }
+}
