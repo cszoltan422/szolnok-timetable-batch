6b70a727822893867fc4e8440e2477d4d6fa1732
Zoltan Csenyi <zoltan_csenyi@epam.com>
2019-10-18 13:44:25 +0200

    - Add jasypt for db password encryption

---
+++ szolnok-timetable-backend-api/src/main/kotlin/org/zenbot/szolnok/timetable/backend/api/configuration/JasyptConfiguration.kt
@@ -0,0 +1,8 @@
+package org.zenbot.szolnok.timetable.backend.api.configuration
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableEncryptableProperties
+class JasyptConfiguration
