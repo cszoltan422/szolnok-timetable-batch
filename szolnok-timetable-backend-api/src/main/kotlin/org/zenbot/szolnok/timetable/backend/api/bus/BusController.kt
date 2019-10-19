64dacdabdea3299f7cc2a9704209c98eed8347a9
Zoltan Csenyi <zoltan_csenyi@epam.com>
2019-10-19 20:23:20 +0200

    - Update buses mapping

--- szolnok-timetable-backend-api/src/main/kotlin/org/zenbot/szolnok/timetable/backend/api/bus/BusController.kt
+++ szolnok-timetable-backend-api/src/main/kotlin/org/zenbot/szolnok/timetable/backend/api/bus/BusController.kt
@@ -3,6 +3,7 @@ package org.zenbot.szolnok.timetable.backend.api.bus
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -10,9 +11,6 @@ class BusController(
     private val busService: BusService
 ) {
 
-    @RequestMapping(value = arrayOf("/api/bus/"), produces = arrayOf("application/json"), method = arrayOf(RequestMethod.GET))
-    fun getAllBuses(): List<BusResponse> = busService.findAll("")
-
-    @RequestMapping(value = arrayOf("/api/bus/{q}"), produces = arrayOf("application/json"), method = arrayOf(RequestMethod.GET))
-    fun getAllBuses(@PathVariable("q") query: String): List<BusResponse> = busService.findAll(query)
+    @RequestMapping(value = arrayOf("/api/bus"), produces = arrayOf("application/json"), method = arrayOf(RequestMethod.GET))
+    fun getAllBuses(@RequestParam("q") q: String?): List<BusResponse> = busService.findAll(q ?: "")
 }
