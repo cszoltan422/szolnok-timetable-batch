1bd1a1c5c81c08d3c472755cbc096a715bfd8f4b
Zoltan Csenyi <zoltan_csenyi@epam.com>
2019-10-22 11:09:08 +0200

    - Remove batch run on startup

--- szolnok-timetable-backend-api/src/main/kotlin/org/zenbot/szolnok/timetable/backend/api/SzolnokTimetableBackendApiApplication.kt
+++ szolnok-timetable-backend-api/src/main/kotlin/org/zenbot/szolnok/timetable/backend/api/SzolnokTimetableBackendApiApplication.kt
@@ -1,21 +1,10 @@
 package org.zenbot.szolnok.timetable.backend.api
 
-import org.springframework.batch.core.Job
-import org.springframework.batch.core.JobParameters
-import org.springframework.batch.core.launch.JobLauncher
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SzolnokTimetableBackendApiApplication(
-    private val szolnokBusesJob: Job,
-    private val jobLauncher: JobLauncher
-) : CommandLineRunner {
-    override fun run(vararg args: String?) {
-        jobLauncher.run(szolnokBusesJob, JobParameters())
-    }
-}
+class SzolnokTimetableBackendApiApplication
 
 fun main(args: Array<String>) {
     runApplication<SzolnokTimetableBackendApiApplication>(*args)
