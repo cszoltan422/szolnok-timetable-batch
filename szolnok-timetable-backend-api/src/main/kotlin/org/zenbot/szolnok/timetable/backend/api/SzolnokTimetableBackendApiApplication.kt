11d7c6eff11c4f88258199370c6597bc92dd1881
Zoltan Csenyi <zoltan_csenyi@epam.com>
2019-10-22 10:07:47 +0200

    - Fall back to mysql   - Add digitalocean db properties   - Run batch on startup

--- szolnok-timetable-backend-api/src/main/kotlin/org/zenbot/szolnok/timetable/backend/api/SzolnokTimetableBackendApiApplication.kt
+++ szolnok-timetable-backend-api/src/main/kotlin/org/zenbot/szolnok/timetable/backend/api/SzolnokTimetableBackendApiApplication.kt
@@ -1,10 +1,21 @@
 package org.zenbot.szolnok.timetable.backend.api
 
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobParameters
+import org.springframework.batch.core.launch.JobLauncher
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SzolnokTimetableBackendApiApplication
+class SzolnokTimetableBackendApiApplication(
+    private val szolnokBusesJob: Job,
+    private val jobLauncher: JobLauncher
+) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        jobLauncher.run(szolnokBusesJob, JobParameters())
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<SzolnokTimetableBackendApiApplication>(*args)
