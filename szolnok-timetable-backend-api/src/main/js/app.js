5bf7f929c51a0841fa255c4a76efd511c4b7fdf9
Zoltan Csenyi <zoltan_csenyi@epam.com>
2019-10-24 16:26:24 +0200

    - Add package.json to api module  - Create react structure in index.html  - Use webpack to create and watch bundle  - Remove header and footer

---
+++ szolnok-timetable-backend-api/src/main/js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const App = require('./component/App');
+
+ReactDOM.render(
+<App />,
+    document.getElementById('root')
+)
---
+++ szolnok-timetable-backend-api/src/main/js/component/App.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+const React = require('react');
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Admin Page v34</div>
+        )
+    }
+}
+
+module.exports = App;
