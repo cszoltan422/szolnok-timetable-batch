a86fb65ff4456500642433b3a7d6581f322fec69
Zoltan Csenyi <zoltan_csenyi@epam.com>
2019-10-25 16:40:10 +0200

    - Move fetching jobs to table component  - Fetch recent jobs every 3 sec

--- szolnok-timetable-backend-api/src/main/js/component/App.jsx
+++ szolnok-timetable-backend-api/src/main/js/component/App.jsx
@@ -1,51 +1,22 @@
 'use strict';
 
-const Spinner = require("react-bootstrap").Spinner;
 const BatchJobListTable = require("./BatchJobListTable");
 const LaunchBatchJobForm = require("./LaunchBatchJobForm");
 const React = require('react');
-const client = require('../client');
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            recentJobs: [],
-            fetched: false
-        }
-    }
-
-    componentDidMount() {
-        client({
-            method: 'GET',
-            path: '/admin/api/jobs/recent',
-            headers: {'Accept': 'application/json'}
-        }).done(response => {
-            this.setState({
-                recentJobs: response.entity,
-                fetched: true
-            })
-        });
     }
 
     render() {
-        let toRender;
-        if (this.state.fetched) {
-            toRender =
-                <div>
-                    <BatchJobListTable jobsList={this.state.recentJobs}/>
-                    <LaunchBatchJobForm />
-                </div>
-        } else {
-            toRender =
-                <div className="main-load-spinner">
-                    <Spinner animation="border" role="status">
-                        <span className="sr-only">Loading...</span>
-                    </Spinner>
-                </div>
-        }
-        return toRender
+        return (
+            <div>
+                <BatchJobListTable />
+                <LaunchBatchJobForm />
+            </div>
+        )
     }
 }
 
--- szolnok-timetable-backend-api/src/main/js/component/BatchJobListTable.jsx
+++ szolnok-timetable-backend-api/src/main/js/component/BatchJobListTable.jsx
@@ -2,34 +2,73 @@
 
 const React = require('react');
 const Table = require("react-bootstrap").Table;
+const Spinner = require("react-bootstrap").Spinner;
 const Badge = require("react-bootstrap").Badge;
 const BatchJobListTableHeader = require("./BatchJobListTableHeader");
 const BatchJobListTableRow = require("./BatchJobListTableRow");
+const client = require('../client');
 
 class BatchJobListTable extends React.Component {
 
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            jobList: [],
+            fetched: false
+        };
+
+        this.fetchJobs = this.fetchJobs.bind(this);
+    }
+
+    componentDidMount() {
+        this.fetchJobs();
+        this.interval = setInterval(() => {
+            this.fetchJobs();
+        }, 3000);
+    }
+
+    fetchJobs() {
+        client({
+            method: 'GET',
+            path: '/admin/api/jobs/recent',
+            headers: {'Accept': 'application/json'}
+        }).done(response => {
+            this.setState({
+                jobList: response.entity,
+                fetched: true
+            })
+        });
     }
 
     render() {
-        return (
-            <div className="batch-jobs-table">
-                <div className="batch-jobs-table-content">
-                    <h2>
-                        <Badge variant="secondary">Recent Jobs</Badge>
-                    </h2>
-                    <Table striped bordered hover>
-                        <BatchJobListTableHeader />
-                        <tbody>
-                        {this.props.jobsList.map(job => (
-                            <BatchJobListTableRow job={job}/>
-                        ))}
-                        </tbody>
-                    </Table>
+        let toRender;
+        if (this.state.fetched) {
+            toRender =
+                <div className="batch-jobs-table">
+                    <div className="batch-jobs-table-content">
+                        <h2>
+                            <Badge variant="secondary">Recent Jobs</Badge>
+                        </h2>
+                        <Table striped bordered hover>
+                            <BatchJobListTableHeader />
+                            <tbody>
+                            {this.state.jobList.map(job => (
+                                <BatchJobListTableRow job={job}/>
+                            ))}
+                            </tbody>
+                        </Table>
+                    </div>
                 </div>
-            </div>
-        )
+        } else {
+            toRender =
+                <div className="main-load-spinner">
+                    <Spinner animation="border" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </Spinner>
+                </div>
+        }
+
+        return toRender
     }
 }
 
