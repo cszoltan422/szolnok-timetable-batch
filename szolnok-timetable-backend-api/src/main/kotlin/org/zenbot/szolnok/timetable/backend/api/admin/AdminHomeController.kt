68524d0e00d64f43056a747fc6641f4346b22412
Zoltan Csenyi <cszoltan422@gmail.com>
2019-11-04 16:32:48 +0100

    - Add heroku home link to header

--- szolnok-timetable-backend-api/src/main/kotlin/org/zenbot/szolnok/timetable/backend/api/admin/AdminHomeController.kt
+++ szolnok-timetable-backend-api/src/main/kotlin/org/zenbot/szolnok/timetable/backend/api/admin/AdminHomeController.kt
@@ -1,15 +1,21 @@
 package org.zenbot.szolnok.timetable.backend.api.admin
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
+import org.zenbot.szolnok.timetable.backend.api.configuration.properties.AdminProperties
 
 @Controller
-class AdminHomeController {
+@EnableConfigurationProperties(AdminProperties::class)
+class AdminHomeController(
+        private val adminProperties: AdminProperties
+) {
 
     @RequestMapping("/")
     fun homepage(): ModelAndView {
         val modelAndView = ModelAndView("index")
+        modelAndView.addObject("applicationConsole", adminProperties.application.applicationConsole)
         return modelAndView
     }
 }
